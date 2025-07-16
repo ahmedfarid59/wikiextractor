ddafd5768ecfa68454f0845e4d1f5fa2a3c650b6
ahmedfarid59 <a.f.elaswar@gmail.com>
2025-07-17 00:40:12 +0300

    final working version

--- run.bat
+++ run.bat
@@ -3,8 +3,8 @@
 python -m wikiextractor.main ^
  arwiki-latest-pages-articles.xml.bz2 ^
        --json ^
-       --processes 1 ^
+       --processes 10 ^
        --templates  templates ^
        --output arwiki-latest-pages-articles ^
-       --bytes 10M ^
+       --bytes 0
        
